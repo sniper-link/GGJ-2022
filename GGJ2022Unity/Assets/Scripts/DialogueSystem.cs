6eaf6f9916e2c46ff1da87c71e9c234c3f76d776
samyeung0722 <samyeung722@gmail.com>
2022-01-29 23:29:05 -0800

    simple dialogue system working

---
+++ GGJ2022Unity/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogueSystem: MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public GameObject dialogueGUI;
+    public Transform dialogueBoxGUI;
+
+    public float letterDelay = 0.1f;
+    public float letterMultiplier = 0.5f;
+
+    public KeyCode DialogueInput = KeyCode.F;
+
+    public string Names;
+
+    public string[] dialogueLines;
+
+    public bool letterIsMultiplied = false;
+    public bool dialogueActive = false;
+    public bool dialogueEnded = false;
+    public bool outOfRange = true;
+
+    public AudioClip audioClip;
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        dialogueText.text = "";
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void EnterRangeOfNPC()
+    {
+        outOfRange = false;
+        dialogueGUI.SetActive(true);
+        if (dialogueActive == true)
+        {
+            dialogueGUI.SetActive(false);
+        }
+    }
+
+    public void NPCName(GameObject NPC)
+    {
+        outOfRange = false;
+        dialogueBoxGUI.gameObject.SetActive(true);
+        nameText.text = Names;
+        if (!dialogueActive)
+        {
+            dialogueActive = true;
+            StartCoroutine(StartDialogue(NPC));
+        }
+        StartDialogue(NPC);
+    }
+
+    private IEnumerator StartDialogue(GameObject NPC)
+    {
+        if (outOfRange == false)
+        {
+            int dialogueLength = dialogueLines.Length;
+            int currentDialogueIndex = 0;
+
+            while (currentDialogueIndex < dialogueLength || !letterIsMultiplied)
+            {
+                if (!letterIsMultiplied)
+                {
+                    letterIsMultiplied = true;
+                    StartCoroutine(DisplayString(dialogueLines[currentDialogueIndex++]));
+
+                    if (currentDialogueIndex >= dialogueLength)
+                    {
+                        dialogueEnded = true;
+                    }
+                }
+                yield return 0;
+            }
+
+            while (true)
+            {
+                if (Input.GetKeyDown(DialogueInput) && dialogueEnded == false)
+                {
+                    break;
+                }
+                yield return 0;
+            }
+            dialogueEnded = false;
+            dialogueActive = false;
+            DropDialogue(NPC);
+        }
+    }
+
+    private IEnumerator DisplayString(string stringToDisplay)
+    {
+        if (outOfRange == false)
+        {
+            int stringLength = stringToDisplay.Length;
+            int currentCharacterIndex = 0;
+
+            dialogueText.text = "";
+
+            while (currentCharacterIndex < stringLength)
+            {
+                dialogueText.text += stringToDisplay[currentCharacterIndex];
+                currentCharacterIndex++;
+
+                if (currentCharacterIndex < stringLength)
+                {
+                    if (Input.GetKey(DialogueInput))
+                    {
+                        yield return new WaitForSeconds(letterDelay * letterMultiplier);
+
+                        if (audioClip) audioSource.PlayOneShot(audioClip, 0.5F);
+                    }
+                    else
+                    {
+                        yield return new WaitForSeconds(letterDelay);
+
+                        if (audioClip) audioSource.PlayOneShot(audioClip, 0.5F);
+                    }
+                }
+                else
+                {
+                    dialogueEnded = false;
+                    break;
+                }
+            }
+            while (true)
+            {
+                if (Input.GetKeyDown(DialogueInput))
+                {
+                    break;
+                }
+                yield return 0;
+            }
+            dialogueEnded = false;
+            letterIsMultiplied = false;
+            dialogueText.text = "";
+        }
+    }
+
+    public void DropDialogue(GameObject NPC)
+    {       
+        dialogueGUI.SetActive(false);
+        dialogueBoxGUI.gameObject.SetActive(false);
+        NPC.GetComponent<NPC>().OutChat();
+    }
+
+    public void OutOfRange()
+    {
+        outOfRange = true;
+        if (outOfRange == true)
+        {
+            letterIsMultiplied = false;
+            dialogueActive = false;
+            StopAllCoroutines();
+            dialogueGUI.SetActive(false);
+            dialogueBoxGUI.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ GGJ2022Unity/Assets/Scripts/NPC.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class NPC : MonoBehaviour {
+
+    public Transform ChatBackGround;
+    public Transform NPCCharacter;
+
+    private DialogueSystem dialogueSystem;
+
+    public string Name;
+
+    [TextArea(5, 10)]
+    public string[] sentences;
+
+    void Start () {
+        dialogueSystem = FindObjectOfType<DialogueSystem>();
+    }
+	
+	void Update () {
+          Vector3 Pos = Camera.main.WorldToScreenPoint(NPCCharacter.position);
+          Pos.y += 175;
+          ChatBackGround.position = Pos;
+    }
+
+    public void Chat()
+    {
+        // this.gameObject.GetComponent<NPC>().enabled = true;
+        // dialogueSystem.EnterRangeOfNPC();
+        dialogueSystem.Names = Name;
+        dialogueSystem.dialogueLines = sentences;
+        dialogueSystem.NPCName(this.gameObject);
+    }
+
+    public void OutChat()
+    {
+        dialogueSystem.OutOfRange();
+        // this.gameObject.GetComponent<NPC>().enabled = false;
+    }
+}
+
