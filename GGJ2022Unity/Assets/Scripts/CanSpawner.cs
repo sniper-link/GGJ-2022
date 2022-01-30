bf7713d95b0689a3a5165f23f12a23989d7c7c8b
sniper_link <44655165+sniper-link@users.noreply.github.com>
2022-01-30 12:30:00 -0800

    add can spawner to gearswell scene

--- GGJ2022Unity/Assets/Scripts/CanSpawner.cs
+++ GGJ2022Unity/Assets/Scripts/CanSpawner.cs
@@ -9,20 +9,20 @@ public class CanSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            SpawnCan();
-        }
+        StartCoroutine(CanSpawnDelay());
     }
 
     public void SpawnCan()
     {
-        Instantiate(canPrefab);
+        Instantiate(canPrefab, transform.position, Quaternion.identity);
+    }
+
+    IEnumerator CanSpawnDelay()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(60);
+            SpawnCan();
+        }
     }
 }
